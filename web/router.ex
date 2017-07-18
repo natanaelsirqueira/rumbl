8e1444493c56fc2dd3eae77f770435966ef1720c
Natanael Sirqueira <natanael.sirqueira@gmail.com>
2017-07-18 11:04:03 -0400

    Building Forms and Creating Resources

--- web/router.ex
+++ web/router.ex
@@ -14,11 +14,10 @@ defmodule Rumbl.Router do
   end
 
   scope "/", Rumbl do
-    pipe_through :browser # Use the default browser stack
-
-    get "/users",     UserController, :index
-    get "/users/:id", UserController, :show
+    pipe_through :browser
+    
     get "/", PageController, :index
+    resources "/users", UserController, only: [:index, :show, :new, :create]
   end
 
   # Other scopes may use custom stacks.
