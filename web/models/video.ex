fb112052afe8c1a18179eed9fb00f7e0ebb36afe
Natanael Sirqueira <natanael.sirqueira@gmail.com>
2017-07-19 15:21:36 -0400

    Watching Videos

--- web/models/video.ex
+++ web/models/video.ex
@@ -1,10 +1,12 @@
 defmodule Rumbl.Video do
   use Rumbl.Web, :model
 
+  @primary_key {:id, Rumbl.Permalink, autogenerate: true}
   schema "videos" do
     field :url, :string
     field :title, :string
     field :description, :string
+    field :slug, :string
     belongs_to :user, Rumbl.User
     belongs_to :category, Rumbl.Category, on_replace: :nilify # anulates the existent relationship
 
@@ -15,11 +17,31 @@ defmodule Rumbl.Video do
   Builds a changeset based on the `struct` and `params`.
   """
   def changeset(struct, category, params \\ %{}) do
-    IO.inspect category
     struct
     |> cast(params, [:url, :title, :description])
     |> validate_required([:url, :title, :description])
+    |> slugify_title()
     |> assoc_constraint(:category)
     |> put_assoc(:category, category)
+  end
+
+  defp slugify_title(changeset) do
+    if title = get_change(changeset, :title) do
+      put_change(changeset, :slug, slugify(title))
+    else
+      changeset
+    end
+  end
+
+  defp slugify(str) do
+    str
+    |> String.downcase()
+    |> String.replace(~r/[^\w-]+/u, "-")
+  end
+end
+
+defimpl Phoenix.Param, for: Rumbl.Video do
+  def to_param(%{slug: slug, id: id}) do
+    "#{id}-#{slug}"
   end
 end
